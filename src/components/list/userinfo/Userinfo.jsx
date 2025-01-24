4193602b7219769eae0d1651fe0cefc2f7565336
Lynx <148461348+LynxLenior@users.noreply.github.com>
2025-01-24 09:11:19 +0000

    Added s in icon

--- src/components/list/userinfo/Userinfo.jsx
+++ src/components/list/userinfo/Userinfo.jsx
@@ -7,7 +7,7 @@ const Userinfo = () => {
         <img src="./avatar.png" alt="" />
         <h2>John Doe </h2>
       </div>
-      <div className="icon">
+      <div className="icons">
         <img src="./more.png" alt="" />
         <img src="./video.png" alt="" />
         <img src="./edit.png" alt="" />
