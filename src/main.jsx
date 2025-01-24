645eb7d2cc14fa0a267a68d2dfe3c3b23b2d8e87
Lynx <148461348+LynxLenior@users.noreply.github.com>
2025-01-24 07:10:39 +0000

    updated main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,39 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap'
+import 'react-bootstrap'
 import './index.css'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+import {
+  createHashRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+// const router = createHashRouter([
+//   {
+//     path: "/", element: <LoginPage />,
+//     errorElement: <ErrorPage />,
+//   },
+//   {
+//     path: "/Home", element: <HomePage />,
+//     errorElement: <ErrorPage />,
+//   },
+//   {
+//     path: "/Dashboard", element: <DBPage />,
+//     children:[
+//       {path: "/Dashboard/Profile", element: <Profile />},
+//       {path: "/Dashboard/Messages", element: <Messages />},
+//       {path: "/Dashboard/Post", element: <Post />},
+//       {path: "/Dashboard/Items", element: <Items />},
+//       {path: "/Dashboard/Requests", element: <Requests />},
+//     ]
+//   },
+// ]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
 )
