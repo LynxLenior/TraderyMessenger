1e8ab1db97d56a652f2fe193e4bea826023660e4
Lynx <148461348+LynxLenior@users.noreply.github.com>
2025-01-24 07:43:39 +0000

    Display Testing

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,10 @@
-import * as React from "react";
-import * as ReactDOM from "react-dom/client";
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap'
-import 'react-bootstrap'
-import './index.css'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import './index.css'
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </React.StrictMode>,
 )
