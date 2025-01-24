77ec9aad44714da4c206c05c7aac50c3705c33a6
Lynx <148461348+LynxLenior@users.noreply.github.com>
2025-01-24 07:51:40 +0000

    added app to main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,19 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import {
+  createHashRouter,
+  RouterProvider,
+} from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createHashRouter ([
+  {
+    path: "/", element: <App />
+  }
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
