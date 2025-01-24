ab024c2c805b272a329a808f42bcf3d982af86d5
Lynx <148461348+LynxLenior@users.noreply.github.com>
2025-01-24 12:45:55 +0000

    Message area

--- src/components/chat/Chat.jsx
+++ src/components/chat/Chat.jsx
@@ -27,7 +27,51 @@ const Chat = () => {
                 <img src="./info.png" alt="" />
             </div>
         </div>
-        <div className="center"></div>
+        <div className="center">
+            <div className="message">
+                <img src="./avatar.png" alt="" />
+                <div className="texts">
+                    <p>
+                        Skibidi Toilet
+                    </p>
+                    <span>1 Min</span>
+                </div>
+            </div>
+            <div className="message own">
+                <div className="texts">
+                    <p>
+                        Skibidi Toilet
+                    </p>
+                    <span>1 Min</span>
+                </div>
+            </div>
+            <div className="message">
+                <img src="./avatar.png" alt="" />
+                <div className="texts">
+                    <p>
+                        Skibidi Toilet
+                    </p>
+                    <span>1 Min</span>
+                </div>
+            </div>
+            <div className="message own">
+                <div className="texts">
+                    <p>
+                        Skibidi Toilet
+                    </p>
+                    <span>1 Min</span>
+                </div>
+            </div>
+            <div className="message">
+                <img src="./avatar.png" alt="" />
+                <div className="texts">
+                    <p>
+                        Skibidi Toilet
+                    </p>
+                    <span>1 Min</span>
+                </div>
+            </div>
+        </div>
         <div className="bottom">
             <div className="icons">
                 <img src="./img.png" alt="" />
