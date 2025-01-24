591bdd8b431a70b4f1d1d5c2a254ff6cc678be2c
Lynx <148461348+LynxLenior@users.noreply.github.com>
2025-01-24 07:26:14 +0000

    Update

--- src/main.jsx
+++ src/main.jsx
@@ -6,32 +6,6 @@ import 'react-bootstrap'
 import './index.css'
 import App from './App.jsx'
 
-import {
-  createHashRouter,
-  RouterProvider,
-} from "react-router-dom";
-
-// const router = createHashRouter([
-//   {
-//     path: "/", element: <LoginPage />,
-//     errorElement: <ErrorPage />,
-//   },
-//   {
-//     path: "/Home", element: <HomePage />,
-//     errorElement: <ErrorPage />,
-//   },
-//   {
-//     path: "/Dashboard", element: <DBPage />,
-//     children:[
-//       {path: "/Dashboard/Profile", element: <Profile />},
-//       {path: "/Dashboard/Messages", element: <Messages />},
-//       {path: "/Dashboard/Post", element: <Post />},
-//       {path: "/Dashboard/Items", element: <Items />},
-//       {path: "/Dashboard/Requests", element: <Requests />},
-//     ]
-//   },
-// ]);
-
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <RouterProvider router={router} />
