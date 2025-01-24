2b878e0659aabbd40e50f9ee5be8309a7c06e3ab
Lynx <148461348+LynxLenior@users.noreply.github.com>
2025-01-24 13:47:18 +0000

    detail area test

--- src/components/detail/Detail.jsx
+++ src/components/detail/Detail.jsx
@@ -2,7 +2,52 @@ import "./detail.css"
 
 const Detail = () => {
   return (
-    <div className='detail'>Detail</div>
+    <div className='detail'>
+        <div className="user">
+            <img src="./avatar.png" alt="" />
+            <h2>Jane Doe</h2>
+            <p>Skibid toilet sigma boi</p>
+        </div>
+        <div className="info">
+            <div className="option">
+              <div className="title">
+                <span>Chat Settings</span>
+                <img src="./arrowUp.png" alt="" />
+                </div>  
+            </div>
+            <div className="option">
+              <div className="title">
+                <span>Privacy % help</span>
+                <img src="./arrowUp.png" alt="" />
+                </div>  
+                <div className="photos">
+                    <div className="photoI">
+                        <img src="https://raw.githubusercontent.com/Sonny4546/Tradery/refs/heads/main/images/background.jpg" alt="" />
+                        <span>photo_2024_2.png</span>
+                    </div>
+                    <img src="./download.png" alt="" />
+                </div>
+            </div>
+            <div className="option">
+              <div className="title">
+                <span>Shared Photos</span>
+                <img src="./arrowDown.png" alt="" />
+                </div>  
+            </div>
+            <div className="option">
+              <div className="title">
+                <span>Chat Settings</span>
+                <img src="./arrowUp.png" alt="" />
+                </div>  
+            </div>
+            <div className="option">
+              <div className="title">
+                <span>Chat Settings</span>
+                <img src="./arrowUp.png" alt="" />
+                </div>  
+            </div>
+        </div>
+    </div>
   )
 }
 
