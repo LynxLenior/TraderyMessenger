5f00f101755f8b4ae3c76a5df037dc5e84eb830e
Lynx <148461348+LynxLenior@users.noreply.github.com>
2025-01-24 09:01:26 +0000

    Profile Looks topleft

--- src/components/list/List.jsx
+++ src/components/list/List.jsx
@@ -1,8 +1,13 @@
+import ChatList from "./chatList/ChatList"
 import "./list.css"
+import Userinfo from "./userinfo/Userinfo"
 
 const List = () => {
   return (
-    <div className=''>List</div>
+    <div className='list'>
+        <Userinfo/>
+        <ChatList/>
+    </div>
   )
 }
 
