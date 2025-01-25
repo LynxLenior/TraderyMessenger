abab9db9252514bb469ef01a053cb358d4f816e5
Lynx <148461348+LynxLenior@users.noreply.github.com>
2025-01-25 12:26:10 +0000

    Vite testing

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: "/TraderyMessenger"
+  build: {
+    chunkSizeWarningLimit: 1000, // Increase to 1MB (or adjust as needed)
+  },
 });
