92f081274642df956f71fd2c891fc55b474c8d25
Lynx <148461348+LynxLenior@users.noreply.github.com>
2025-01-25 10:17:38 +0000

    LoginComponents

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,9 +1,27 @@
 import "./login.css"
 
 const Login = () => {
-  return (
-    <div className='login'>Login</div>
-  )
+  return <div className='login'>
+        <div className="item">
+            <h2>Welcome back,</h2>
+            <form>
+                <input type="text" placeholder="Email" name="email"/>
+                <input type="text" placeholder="Password" name="password"/>
+                <button>Sign In</button>
+            </form>
+        </div>
+        <div className="separator"></div>
+        <div className="item">
+        <h2>Create an Account</h2>
+            <form>
+                <input type="file" id="file"/>
+                <input type="text" placeholder="Username" name="username"/>
+                <input type="text" placeholder="Email" name="email"/>
+                <input type="password" placeholder="Password" name="password"/>
+                <button>Sign In</button>
+            </form>
+        </div>
+    </div>
 }
 
 export default Login
