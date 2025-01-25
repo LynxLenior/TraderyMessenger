542d698a7959e0d7c7aa5171a33f38f086c85510
Lynx <148461348+LynxLenior@users.noreply.github.com>
2025-01-25 10:39:58 +0000

    Select Image

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,6 +1,21 @@
+import { useState } from "react"
 import "./login.css"
-
+//Login thingies, delete for GoogleAuthenticator login thingy
 const Login = () => {
+    const [avatar,setAvatar] = useState({
+        file:null,
+        url:""
+    })
+
+    const handleAvatar = e =>{
+        if(e.target.files[0]){
+        
+        setAvatar({
+            file:e.target.files[0],
+            url: URL.createObjectURL(e.target.files[0])
+        })
+    }
+}
   return <div className='login'>
         <div className="item">
             <h2>Welcome back,</h2>
@@ -14,11 +29,14 @@ const Login = () => {
         <div className="item">
         <h2>Create an Account</h2>
             <form>
-                <input type="file" id="file"/>
+                <label htmlFor="file">
+                    <img src={avatar.url || "./avatar.png"} alt="" />
+                    Upload and image</label>
+                <input type="file" id="file" style={{display:"none"}} onChange={handleAvatar}/>
                 <input type="text" placeholder="Username" name="username"/>
                 <input type="text" placeholder="Email" name="email"/>
                 <input type="password" placeholder="Password" name="password"/>
-                <button>Sign In</button>
+                <button>Sign up</button>
             </form>
         </div>
     </div>
