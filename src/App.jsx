f2606765aa25acb1d1e1d8ab8f221adc7d540bc1
Lynx <148461348+LynxLenior@users.noreply.github.com>
2025-01-27 13:34:13 +0000

    Reverted

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,18 @@
-import { onAuthStateChanged } from "firebase/auth";
 import Chat from "./components/chat/Chat";
 import Detail from "./components/detail/Detail";
 import List from "./components/list/List";
 import Login from "./components/login/Login";
 import Notification from "./components/notification/Notification";
-import { useEffect } from "react";
-import { auth } from "./lib/firebase";
-import { useUserStore } from "./lib/userStore";
+
 
 const App = () => {
-
-  const {currentUser, isLoading, fetchUserInfo } = useUserStore()
-
-  useEffect(()=>{
-    const unSub = onAuthStateChanged(auth,(user)=>{
-      fetchUserInfo(user.uid);
-    })
-
-    return () => {
-      unSub()
-    }
-  }, [fetchUserInfo])
-
-  console.log(currentUser)
-
-  if (isLoading) return <div className="loading">Loading...</div>
+//open login page = false close = true
+const user = false
 
   return (
     <div className='container'>
-      {currentUser ? (
+      {
+        user ? (
           <>
           <List/>
           <Chat/>
