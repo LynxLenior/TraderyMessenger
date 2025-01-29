59c2aaae6d69313febafb3fc1c3f5f62bf3b05a4
Lynx <148461348+LynxLenior@users.noreply.github.com>
2025-01-29 09:42:13 +0000

    GOING TO MESSAGES PLSSSS

--- src/App.jsx
+++ src/App.jsx
@@ -6,33 +6,39 @@ import Login from "./components/login/Login";
 import Notification from "./components/notification/Notification";
 import { auth } from "./lib/firebase";
 import { onAuthStateChanged } from "firebase/auth";
+import { useUserStore } from "./lib/userStore";
 
 
 const App = () => {
 //open login page = false close = true
-const user = false
+
+  const {currentUser, isLoading, fetchUserInfo} = useUserStore()
 
   useEffect(() =>{
     const unSub = onAuthStateChanged(auth, (user)=>{
-      console.log(user)
+      fetchUserInfo(user.uid)
     })
 
     return () => {
       unSub()
     }
-  }, [] )
+  }, [fetchUserInfo] )
+
+  console.log(currentUser)
+
+  if (isLoading) return <div className="loading">Loading...</div>
 
   return (
     <div className='container'>
-      {
-        user ? (
+      {currentUser ? (
           <>
           <List/>
           <Chat/>
           <Detail/>
           </>
-        ) : <Login/>
-      }
+        ) : ( 
+        <Login/>
+      )}
       <Notification/>
     </div>
   )
