26f45f5b7a0702fc566d079a8c98fd9f93058cf3
Lynx <148461348+LynxLenior@users.noreply.github.com>
2025-01-29 13:52:09 +0000

    ADDING USER CHAT IN CHATLIST

--- src/components/list/chatList/addUser/addUser.jsx
+++ src/components/list/chatList/addUser/addUser.jsx
@@ -2,20 +2,23 @@ import { useState } from "react"
 import { db } from "../../../../lib/firebase"
 import "./addUser.css"
 import { 
+  arrayUnion,
   collection,  
   doc,  
   getDocs, 
   query, 
   serverTimestamp, 
   setDoc, 
+  updateDoc, 
   where,
 } from "firebase/firestore"
+import { useUserStore } from "../../../../lib/userStore"
 
 
 const AddUser = () => {
   const [user, setUser] = useState(null)
 
-  
+  const {currentUser} = useUserStore
 
   const handleSearch = async e=> {
     e.preventDefault()
@@ -47,6 +50,24 @@ const AddUser = () => {
        await setDoc(newChatRef, {
         createdAt: serverTimestamp(),
         messages: [],
+      })
+
+      await updateDoc(doc(userChatsRef, user.id), {
+        chats:arrayUnion({
+          chatId: newChatRef.id,
+          lastMessage:"",
+          receiverId: currentUser.id,
+          updatedAt: Date.now(),
+        })
+      })
+
+      await updateDoc(doc(userChatsRef, currentUser.id), {
+        chats:arrayUnion({
+          chatId: newChatRef.id,
+          lastMessage:"",
+          receiverId: user.id,
+          updatedAt: Date.now(),
+        })
       })
 
       console.log(newChatRef.id)
