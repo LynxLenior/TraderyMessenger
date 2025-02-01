f94df8540212ebd24955c7003a6afcaa75a62233
Lynx <148461348+LynxLenior@users.noreply.github.com>
2025-02-01 06:38:56 +0000

    bgcolour

--- src/components/list/chatList/ChatList.jsx
+++ src/components/list/chatList/ChatList.jsx
@@ -58,7 +58,11 @@ const ChatList = () => {
             />
         </div>
         {chats.map((chat) => (
-        <div className="item" key={chat.chatId} onClick={()=>handleSelect(chat)}>
+        <div className="item" key={chat.chatId} onClick={()=>handleSelect(chat)}
+            style={{
+                backgroundColor: chat?.isSeen ? "transparent" : "#5183fe"
+            }}
+        >
             <div className="texts">
                 <span>{chat.user.username}</span>
                 <p>{chat.lastMessage}</p>
