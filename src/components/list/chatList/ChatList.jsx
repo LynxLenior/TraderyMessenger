d4a703806b254af3be8ffa239d6896d549ae80a3
Lynx <148461348+LynxLenior@users.noreply.github.com>
2025-02-01 06:53:31 +0000

    Blue bg is seen

--- src/components/list/chatList/ChatList.jsx
+++ src/components/list/chatList/ChatList.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react"
 import "./chatList.css"
 import AddUser from "./addUser/addUser";
 import { useUserStore } from "../../../lib/userStore";
-import { doc, getDoc, onSnapshot } from "firebase/firestore";
+import { doc, getDoc, onSnapshot, updateDoc } from "firebase/firestore";
 import { db } from "../../../lib/firebase";
 import { useChatStore } from "../../../lib/chatStore";
 
@@ -37,10 +37,31 @@ const ChatList = () => {
     }, [currentUser.id])
 
     const handleSelect = async (chat) =>{
+        
+        const userChats = chats.map(item=>{
+            const {user, ...rest } = item
+            return rest
+        })
 
-        changeChat(chat.chatId,chat.user)
+        const chatIndex = userChats.findIndex(item=>item.chatId === chat.chatId)
 
+        userChats[chatIndex].isSeen = true
+
+        const userChatsRef = doc(db, "userchats", currentUser.id)
+
+        try{
+
+            await updateDoc(userChatsRef,{
+               chats:userChats, 
+            })
+            changeChat(chat.chatId,chat.user)
+        }catch(err){
+            console.log(err)
+        }
     }
+            
+
+
 
     
 
