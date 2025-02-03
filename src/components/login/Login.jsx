11cd4a2feeeb30546c077f76a5007689d218d3b3
Lynx <148461348+LynxLenior@users.noreply.github.com>
2025-02-03 12:28:57 +0000

    Testing Testing

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -7,106 +7,110 @@ import { doc, setDoc } from "firebase/firestore";
 
 //Login thingies, delete for GoogleAuthenticator login thingy
 const Login = () => {
-    const [avatar,setAvatar] = useState({
-        file:null,
-        url:"",
-    })
+//     const [avatar,setAvatar] = useState({
+//         file:null,
+//         url:"",
+//     })
 
-    const [loading,setLoading] = useState(false)
+//     const [loading,setLoading] = useState(false)
 
-    const handleAvatar = e => {
-        if (e.target.files[0]){
-            setAvatar({
-                file: e.target.files[0],
-                url: URL.createObjectURL(e.target.files[0]),
-        })
-    }
-}
+//     const handleAvatar = e => {
+//         if (e.target.files[0]){
+//             setAvatar({
+//                 file: e.target.files[0],
+//                 url: URL.createObjectURL(e.target.files[0]),
+//         })
+//     }
+// }
 
-//error popout
-    const handleRegister = async e =>{
-        e.preventDefault()
-        setLoading(true)
-        const formData = new FormData(e.target)
+// //error popout
+//     const handleRegister = async e =>{
+//         e.preventDefault()
+//         setLoading(true)
+//         const formData = new FormData(e.target)
 
-        const { username, email, password } = Object.fromEntries(formData)
+//         const { username, email, password } = Object.fromEntries(formData)
 
-        try{
+//         try{
 
-        const res = await createUserWithEmailAndPassword(auth, email, password)
+//         const res = await createUserWithEmailAndPassword(auth, email, password)
 
-        await setDoc(doc(db, "users", res.user.uid), {
-            username,
-            email,
-            id: res.user.uid,
-            blocked: [],
-        });
+//         await setDoc(doc(db, "users", res.user.uid), {
+//             username,
+//             email,
+//             id: res.user.uid,
+//             blocked: [],
+//         });
 
-        await setDoc(doc(db, "userchats", res.user.uid), {
-            chats: [],
-          });
+//         await setDoc(doc(db, "userchats", res.user.uid), {
+//             chats: [],
+//           });
            
-          toast.success("Account Created! You can login now!")
-        }   catch(err){
-            console.log(err)
-            toast.error(err.message)
-        } finally{
-            setLoading(false)
-        }
-    }
+//           toast.success("Account Created! You can login now!")
+//         }   catch(err){
+//             console.log(err)
+//             toast.error(err.message)
+//         } finally{
+//             setLoading(false)
+//         }
+//     }
 
 
-    const handleLogin = async e =>{
-        e.preventDefault()
-        setLoading(true)
+//     const handleLogin = async e =>{
+//         e.preventDefault()
+//         setLoading(true)
 
-        const formData = new FormData(e.target)
-        const { email, password } = Object.fromEntries(formData)
+//         const formData = new FormData(e.target)
+//         const { email, password } = Object.fromEntries(formData)
 
-        try{
+//         try{
 
-        await signInWithEmailAndPassword(auth, email, password)
-        }catch(err){
-            console.log(err)
-            toast.error(err.message)
-        }
-        finally{
-            setLoading(false)
-        }
-    }
+//         await signInWithEmailAndPassword(auth, email, password)
+//         }catch(err){
+//             console.log(err)
+//             toast.error(err.message)
+//         }
+//         finally{
+//             setLoading(false)
+//         }
+//     }
 
-  return (
         <div className="login">
-        <div className="item">
-            <h2>Welcome back,</h2>
-            <form onSubmit={handleLogin}>
-                <input type="text" placeholder="Email" name="email" />
-                <input type="password" placeholder="Password" name="password"/>
-                <button disabled={loading}>{loading ? "Loading" :"Sign In"}</button>
-            </form>
+            <button>Buttones</button>
         </div>
-        <div className="separator"></div>
-        <div className="item">
-        <h2>Create an Account</h2>
-            <form onSubmit={handleRegister}>
-                <label htmlFor="file">
-                    <img src={avatar.url || "./avatar.png"} alt="" />
-                    Upload and image
-                </label>
-                <input 
-                type="file" 
-                id="file" 
-                style={{display:"none"}} 
-                onChange={handleAvatar}
-                />
-                <input type="text" placeholder="Username" name="username"/>
-                <input type="text" placeholder="Email" name="email"/>
-                <input type="password" placeholder="Password" name="password"/>
-                <button disabled={loading}>{loading ? "Loading" :"Sign up"}</button>
-            </form>
-        </div>
-    </div>
-  )
+
+//   return (
+//         <div className="login">
+//         <div className="item">
+//             <h2>Welcome back,</h2>
+//             <form onSubmit={handleLogin}>
+//                 <input type="text" placeholder="Email" name="email" />
+//                 <input type="password" placeholder="Password" name="password"/>
+//                 <button disabled={loading}>{loading ? "Loading" :"Sign In"}</button>
+//             </form>
+//         </div>
+//         <div className="separator"></div>
+//         <div className="item">
+//         <h2>Create an Account</h2>
+//             <form onSubmit={handleRegister}>
+//                 <label htmlFor="file">
+//                     <img src={avatar.url || "./avatar.png"} alt="" />
+//                     Upload and image
+//                 </label>
+//                 <input 
+//                 type="file" 
+//                 id="file" 
+//                 style={{display:"none"}} 
+//                 onChange={handleAvatar}
+//                 />
+//                 <input type="text" placeholder="Username" name="username"/>
+//                 <input type="text" placeholder="Email" name="email"/>
+//                 <input type="password" placeholder="Password" name="password"/>
+//                 <button disabled={loading}>{loading ? "Loading" :"Sign up"}</button>
+//             </form>
+//         </div>
+//     </div>
+//   )
 }
 
 export default Login
