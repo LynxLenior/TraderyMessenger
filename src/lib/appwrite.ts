58707b0c03ace068df3b62aca1054c706ee72d1d
Saiki <76763287+Sonny4546@users.noreply.github.com>
2025-03-15 17:40:48 +0800

    add vite-env, fix appwrite ts

--- src/lib/appwrite.ts
+++ src/lib/appwrite.ts
@@ -1,5 +1,4 @@
-import { useNavigate } from "react-router-dom";
-import { Account, Client, OAuthProvider, Storage, Databases, Teams } from 'appwrite'
+import { Account, Client, OAuthProvider, Storage, Databases, Models } from 'appwrite'
 
 export const client = new Client()
 client
@@ -9,6 +8,13 @@ export { OAuthProvider }
 
 export const storage = new Storage(client);
 export const database = new Databases(client);
+export const account = new Account(client);
+
+export interface TraderyUser {
+  name: string;
+  $id: string;
+  email: string;
+}
 
 export async function getCurrentSession() {
   const session = await account.getSession('current');
@@ -24,16 +30,6 @@ export const getUser = async () => {
     console.error(error)
   }
 };
-
-export async function DeleteSession() {
-  await account.deleteSession('current');
-  console.log("log out");
-}
-
-export async function getUserPrefs() {
-  const data = await account.getPrefs();
-  console.log(data);
-}
 
 export async function findUserDataById(userId) {
     const document = await database.getDocument(
