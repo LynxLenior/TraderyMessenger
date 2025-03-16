26e6758a7438a3a0685b2cc509c5418c9097723e
Saiki <76763287+Sonny4546@users.noreply.github.com>
2025-03-16 17:44:32 +0800

    fix name

--- src/lib/appwrite.ts
+++ src/lib/appwrite.ts
@@ -47,7 +47,7 @@ export async function findUserDataById(userId) {
 
 function mapUserToItem(document: Models.Document) {
   const userdb: TraderyProfiles = {
-    defaultName: document.name,
+    defaultName: document.defaultName,
     userId: document.$id,
     userEmail: document.userEmail,
     profileImageId: '',
