44cb76f2036bfc06894d4e74c2705d8071209649
LynxLenior <anselliambagus930@gmail.com>
2025-03-19 12:26:20 +0800

    Import disappeared the fuc

--- src/components/list/chatList/ChatList.jsx
+++ src/components/list/chatList/ChatList.jsx
@@ -1,3 +1,11 @@
+import { useEffect, useState } from "react"
+import "./chatList.css"
+import AddUser from "./addUser/addUser";
+import { useUserStore } from "../../../lib/userStore";
+import { doc, getDoc, onSnapshot, updateDoc } from "firebase/firestore";
+import { db } from "../../../lib/firebase";
+import { useChatStore } from "../../../lib/chatStore";
+
 const ChatList = ({ onSelectChat }) => {
     const [chats, setChats] = useState([]);
     const [addMode, setAddMode] = useState(false);
@@ -54,30 +62,30 @@ const ChatList = ({ onSelectChat }) => {
     );
 
     return (
-        <div className={`chatList ${isChatOpen ? "hidden" : ""}`}>
-            <button onClick={onSelectChat}>Open Chat</button>
+        <div className='chatList'>
             <div className="search">
                 <div className="searchBar">
                     <img src="./search.png" alt="" />
                     <input type="text" placeholder="Search" onChange={(e) => setInput(e.target.value)} />
                 </div>
-                <img src={addMode ? "./minus.png" : "./plus.png"} 
-                    alt="" 
-                    className="add" 
+                <img
+                    src={addMode ? "./minus.png" : "./plus.png"}
+                    alt=""
+                    className="add"
                     onClick={() => setAddMode((prev) => !prev)}
                 />
             </div>
             {filteredChats.map((chat) => (
-                <div 
-                    className="item" 
-                    key={chat.chatId} 
+                <div
+                    className="item"
+                    key={chat.chatId}
                     onClick={() => handleSelect(chat)}
-                    style={{
-                        backgroundColor: chat?.isSeen ? "transparent" : "#5183fe",
-                    }}
+                    style={{ backgroundColor: chat?.isSeen ? "transparent" : "#5183fe" }}
                 >
                     <div className="texts">
-                        <span>{chat.user.blocked.includes(currentUser.id) ? "User" : chat.user.username}</span>
+                        <span>
+                            {chat.user.blocked.includes(currentUser.id) ? "User" : chat.user.username}
+                        </span>
                         <p>{chat.lastMessage}</p>
                     </div>
                 </div>
@@ -86,3 +94,5 @@ const ChatList = ({ onSelectChat }) => {
         </div>
     );
 };
+
+export default ChatList
